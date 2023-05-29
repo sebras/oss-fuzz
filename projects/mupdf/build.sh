6daca570d9e7c8ef75c93061575c5f34117b4bdd
Sebastian Rasmussen <sebras@gmail.com>
2023-05-29 13:04:24 +0200

    mupdf-build-verbose

--- projects/mupdf/build.sh
+++ projects/mupdf/build.sh
@@ -18,7 +18,7 @@
 # supp_size is unused in harfbuzz so we will avoid it being unused.
 sed -i 's/supp_size;/supp_size;(void)(supp_size);/g' ./thirdparty/harfbuzz/src/hb-subset-cff1.cc
 
-LDFLAGS="$CXXFLAGS" make -j$(nproc) HAVE_GLUT=no build=debug OUT=$WORK \
+LDFLAGS="$CXXFLAGS" make -j$(nproc) HAVE_GLUT=no build=debug OUT=$WORK verbose=yes \
     $WORK/libmupdf-third.a $WORK/libmupdf.a
 fuzz_target=pdf_fuzzer
 
